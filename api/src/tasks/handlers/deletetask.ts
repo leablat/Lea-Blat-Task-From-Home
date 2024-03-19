dd0ae70bb8543e1f01a2059e7ad8c8955bf688d5
lea blat <l0527123860@gmail.com>
2024-03-20 00:06:50 +0200

    end of task

--- api/src/tasks/handlers/deletetask.ts
+++ api/src/tasks/handlers/deletetask.ts
@@ -3,17 +3,17 @@ import { tasksModel } from '../../database/models/taskModel';
 
 
 async function deleteOfTask(req: Request, res: Response, next: NextFunction) {
-   
-    try {
-        const taskId = req.params.id;
-    
-        await tasksModel.findByIdAndDelete(taskId);
-    
-        res.sendStatus(200);
-      } catch (error) {
-        console.error('Error deleting a task:', error);
-        res.status(500).send('Internal Server Error'); // Sending error response
-      }
+
+  try {
+    const taskId = req.params.id;
+
+    await tasksModel.findByIdAndDelete(taskId);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error deleting a task:', error);
+    res.status(500).send('Internal Server Error');
+  }
 }
 
 export { deleteOfTask };
--- api/src/tasks/handlers/editTask.ts
+++ api/src/tasks/handlers/editTask.ts
@@ -3,23 +3,21 @@ import { tasksModel } from '../../database/models/taskModel';
 import Joi from 'joi';
 
 const taskSchema = Joi.object({
-    Description: Joi.string().required(),
-    Date: Joi.date().iso().required(),
-    Status: Joi.boolean()
+  Description: Joi.string().required(),
+  Date: Joi.date().iso().required(),
+  Status: Joi.boolean()
 });
 
 async function updateTask(req: Request, res: Response, next: NextFunction) {
-   
-    try {
-        const taskId = req.params.id;
-        const update = req.body;
 
-       await tasksModel.findByIdAndUpdate(taskId, update);
-
-    res.sendStatus(200); 
+  try {
+    const taskId = req.params.id;
+    const update = req.body;
+    await tasksModel.findByIdAndUpdate(taskId, update);
+    res.sendStatus(200);
   } catch (error) {
     console.error('Error editing a task:', error);
-    res.status(500).send('Internal Server Error'); 
+    res.status(500).send('Internal Server Error');
   }
 }
 
